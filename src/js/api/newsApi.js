5a25107e96c0e022fc78a1b844a5e4516f55e3c3
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-03 15:52:21 +0100

    Set Up Flux Architecture

---
+++ src/js/api/newsApi.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const getSources = (source, sortBy, callback) => {
+    const all = sources;
+    const api = 'https://newsapi.org/v1/'
+    const key = '213327409d384371851777e7c7f78dfe';
+
+    if(source === null){
+        const link = api + all
+    }
+}
