11302b30b8056015c2b65c207154d1f715ca052d
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-03 13:05:42 +0100

    Add Pages

--- src/js/client.js
+++ src/js/client.js
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Router, Route } from 'react-router'
 
-class Main extends React.Component {
-    render() {
-        return (
-            <h1>Hello World!!!</h1>
-        );
-    }
-}
-
-class Headlines extends React.Component {
-    render(){
-        return (
-            <h1>Hello my Name</h1>
-        )
-    }
-}
+import Headline from './pages/Headline';
+import Main from './pages/Main.js';
 
 const app = document.getElementById('app');
 ReactDom.render(<Router>
     <Route path="/" component={Main} />
+    <Route path="/headline" component={Headline} />
 </Router>
     , app)
